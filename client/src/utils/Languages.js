636322470dabb9554f590c28711777ea80b036be
dylanallgood <dylanblakeallgood@gmail.com>
2020-12-16 10:40:44 -0500

    created helper objs

---
+++ client/src/utils/Languages.js
@@ -0,0 +1,44 @@
+let langs = {
+  English: 'en',
+  Chinese: 'zh-tw',
+  French: 'fr',
+  Spanish: 'es',
+  Russian: 'ru',
+  Japanese: 'ja',
+  Korean: 'ko',
+  German: 'de',
+  Arabic: 'ar',
+  Danish: 'da',
+  Dutch: 'nl',
+  Finnish: 'fi',
+  Italian: 'it',
+  Norwegian: 'no',
+  Polish: 'pl',
+  Portuguese: 'pt',
+  Swedish: 'sv',
+  Thai: 'th',
+};
+
+let speechLangs = {
+  Korean: { name: 'Jihun', type: 'ko_kr' },
+  Spanish: { name: 'Gloria', type: 'es_mx' },
+  Chinese: { name: 'Yafang', type: 'zh_tw' },
+  Swedish: { name: 'Sven', type: 'sv_se' },
+  Japanese: { name: 'Akira', type: 'ja_jp' },
+  Arabic: { name: 'Amir', type: 'ar_ar' },
+  Polish: { name: 'Ania', type: 'pl_pl' },
+  French: { name: 'Benoit', type: 'fr_fr' },
+  Finnish: { name: 'Elina', type: 'fi_fi' },
+  Italian: { name: 'Elisa', type: 'it_it' },
+  Dutch: { name: 'Guus', type: 'nl_nl' },
+  Portuguese: { name: 'Helena', type: 'pt_br' },
+  German: { name: 'Lena', type: 'de_de' },
+  Danish: { name: 'Lene', type: 'da_dk' },
+  Norwegian: { name: 'lykke', type: 'no_nb' },
+  Thai: { name: 'Sarawut', type: 'th_th' },
+};
+
+module.exports = {
+  langs,
+  speechLangs,
+};
